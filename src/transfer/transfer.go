eeee9e32366108c7130a27f611433dcf65c21be6
mananchayaaoy <mananchaya.amorn@gmail.com>
2018-07-04 15:55:21 +0700

    [Added]: สร้าง ฟังก์ชัน Test Test_checkBalance_Input_20000_Dot_00_And_500_Dot_00_And_0_Dot_00_Should_Be_True and Test_checkBalance_Input_20000_Dot_00_And_30000_Dot_00_And_0_Dot_00_Should_Be_False และ สร้างฟังก์ชัน checkBalance

--- src/transfer/transfer.go
+++ src/transfer/transfer.go
@@ -1,1 +1,9 @@
 package transfer
+
+func checkBalance(amount, transterMoney, fee float64) bool {
+	if transterMoney+fee >= amount {
+		return false
+	}
+
+	return true
+}
--- src/transfer/transfer_test.go
+++ src/transfer/transfer_test.go
@@ -1,1 +1,31 @@
 package transfer
+
+import "testing"
+
+func Test_checkBalance_Input_20000_Dot_00_And_500_Dot_00_And_0_Dot_00_Should_Be_True(t *testing.T) {
+	amount := 20000.00
+	fee := 0.00
+	tarnsferMoney := 500.00
+	expected := true
+
+	actual := checkBalance(amount, tarnsferMoney, fee)
+
+	if expected != actual {
+		t.Errorf("Should be %v but got %v", expected, actual)
+	}
+
+}
+
+func Test_checkBalance_Input_20000_Dot_00_And_30000_Dot_00_And_0_Dot_00_Should_Be_False(t *testing.T) {
+	amount := 20000.00
+	fee := 0.00
+	tarnsferMoney := 30000.00
+	expected := false
+
+	actual := checkBalance(amount, tarnsferMoney, fee)
+
+	if expected != actual {
+		t.Errorf("Should be %v but got %v", expected, actual)
+	}
+
+}
