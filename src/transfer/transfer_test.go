e7338daf1182a94b3ddbc7a7b627d01a3091bff5
teerawat104 <jjsweet104@gmail.com>
2018-07-04 15:44:59 +0700

    [Created]:สร้าง functions getAccount

--- src/transfer/transfer_test.go
+++ src/transfer/transfer_test.go
@@ -1,1 +1,28 @@
 package transfer
+
+import "testing"
+
+func Test_getAccount_Input_981751424_Should_Be_Panumars_Seanto(t *testing.T) {
+
+	accountNumber := "981751424"
+	expected := Account{Name: "Panumars Seanto", Id: "981751424", Balance: 20000.00, TransferPerDay: 0.00}
+
+	actual := getAccount(accountNumber)
+
+	if expected != actual {
+		t.Errorf("Should be %v but got %v", expected, actual)
+	}
+
+}
+func Test_getAccount_Input_981751425_Should_Be_Piyanuch_Ekpiyakool(t *testing.T) {
+
+	accountNumber := "981751425"
+	expected := Account{Name: "Piyanuch Ekpiyakool", Id: "981751425", Balance: 100000.00, TransferPerDay: 0.00}
+
+	actual := getAccount(accountNumber)
+
+	if expected != actual {
+		t.Errorf("Should be %v but got %v", expected, actual)
+	}
+
+}
