342ebe5ed70d856303f753af07b850bc51a8322f
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-07-04 15:52:56 +0700

    [Added]: ฟังก์ชัน TransferService ในไฟล์ transfer.go

--- src/transfer/transfer.go
+++ src/transfer/transfer.go
@@ -1,1 +1,15 @@
 package transfer
+
+type TransferResponse struct {
+	BalanceOld float64
+	BalanceNew float64
+	Withdrawal float64
+}
+
+func TransferService(accountNumberFrom, accountNumberTo string, amountTransfer float64) TransferResponse {
+	return TransferResponse{
+		BalanceOld: 20000.00,
+		BalanceNew: 19500.00,
+		Withdrawal: 500.00,
+	}
+}
--- src/transfer/transfer_test.go
+++ src/transfer/transfer_test.go
@@ -1,1 +1,18 @@
 package transfer
+
+import "testing"
+
+func Test_TransferService_Input_AccountNumberFrom_9817571424_And_AccountNumberTo_9817571425_And_AmountTransfer_500_Should_Be_TransferResponse(t *testing.T) {
+	accountNumberFrom := "9817571424"
+	accountNumberTo := "9817571425"
+	amountTransfer := 500.00
+	expected := TransferResponse{
+		BalanceOld: 20000.00, BalanceNew: 19500.00, Withdrawal: 500.00,
+	}
+
+	actual := TransferService(accountNumberFrom, accountNumberTo, amountTransfer)
+
+	if expected != actual {
+		t.Errorf("Should Be\n%v but it got\n%v", expected, actual)
+	}
+}
